3425cba35c35719fee7f5946bde694a4f6e96ef1
Motasem Hamed <motasem.hamed@kryptonic.io>
2024-10-28 14:42:12 +0200

    check publish

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
 const val PUBLISHING_GROUP = "com.github.motasem91.librarydemo"
 
 object AppCoordinates {
-    const val APP_ID = "com.github.motasem91.librarydemo.app"
+    const val APP_ID = "com.github.motasem91.librarydemo"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
